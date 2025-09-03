85988369a7e66b43e4113ba19baa871ea4655913
nekena86 <hei.nekena.6@gmail.com>
2025-09-03 12:16:41 +0300

    ajout de reponse

--- main.py
+++ main.py
@@ -1,8 +1,50 @@
+import code
+from tokenize import String
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from starlette.responses import JSONResponse
 
 app = FastAPI()
 
+list:object = []
+
+class Characteristic:
+    max_speed:int
+    max_fuel_capacity:int
 
 @app.get("/ping")
-async def root():
+def ping():
     return {"message": "pong"}
+
+object (BaseModel)
+  id:String
+  brand:String
+  model:String
+  characteristique:Characteristic
+
+
+@app.post("/cars")
+def cars():
+    return {}
+
+@app.get("/cars")
+def read_car():
+    return {object}
+
+@app.get("/cars/{id}")
+def read_car(id:String):
+    if id in list:
+        return {JSONResponse(object)}
+    else:
+        return {JSONResponse("id fourni n'existe pas ou n'a pas ete trouve",status_code=404)}
+
+
+@app.put("/cars/{id}/characteristics")
+def update_characteristics():
+    return {}
+
+
+
+
